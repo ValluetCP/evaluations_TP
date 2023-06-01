87fa43e40260ded8511cfc04ea1299cccd0cc1a2
Cynthia <cynthia.petitot@gmail.com>
2023-06-01 19:18:34 +0200

    flèche du haut - essai 1

---
+++ TP2_restaurant-septime/js/app.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn_f', '.icone');
+
+btn.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
