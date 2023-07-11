881f3b24df18706ba63458727f181dc8fe6182da
CYNTHIA <cynthia.petitot@gmail.com>
2023-07-11 15:36:23 +0200

    WF3 - évaluation 2 (suite)

--- WF3_evaluation_2/WF3_evaluation2/js/sujet.js
+++ WF3_evaluation_2/WF3_evaluation2/js/sujet.js
@@ -23,7 +23,22 @@ setInterval(() => {
     "url(" + images[curentIndex] + ")"; // (version2)
 }, 15000);
 
+// DATE PICKIER
+
+$(document).ready(function () {
+  $("#defaultDate").datepicker();
+});
+
+
+/* SLIDE TOGGLE */
+
+$("#toggleButton").click(function () {
+  $(".myDiv").slideToggle();
+});
+
 /* CAROUSSEL */
+
+// voiture 1
 let curentIndex2 = 0;
 
 let image = [
@@ -57,8 +72,77 @@ $(".next").click(() => {
 });
 
 
-// DATE PICKIER
 
-$(document).ready(function () {
-  $("#defaultDate").datepicker();
+// voiture 2
+$(".prev2").click(() => {
+  curentIndex2--;
+  if (curentIndex2 < 0) {
+    curentIndex2 = image.length - 1;
+  }
+  $(".bg2").css({
+    backgroundImage: "url(" + image[curentIndex2] + ")",
+  });
+
+  console.log(image[curentIndex2]);
+});
+
+$(".next2").click(() => {
+  curentIndex2++;
+  if (curentIndex2 == image.length) {
+    curentIndex2 = 0;
+  }
+  $(".bg2").css({
+    backgroundImage: "url(" + image[curentIndex2] + ")",
+  });
+  console.log(image[curentIndex2]);
+});
+
+
+// voiture 3
+$(".prev3").click(() => {
+  curentIndex2--;
+  if (curentIndex2 < 0) {
+    curentIndex2 = image.length - 1;
+  }
+  $(".bg3").css({
+    backgroundImage: "url(" + image[curentIndex2] + ")",
+  });
+
+  console.log(image[curentIndex2]);
+});
+
+$(".next3").click(() => {
+  curentIndex2++;
+  if (curentIndex2 == image.length) {
+    curentIndex2 = 0;
+  }
+  $(".bg3").css({
+    backgroundImage: "url(" + image[curentIndex2] + ")",
+  });
+  console.log(image[curentIndex2]);
+});
+
+
+// voiture 4
+$(".prev4").click(() => {
+  curentIndex2--;
+  if (curentIndex2 < 0) {
+    curentIndex2 = image.length - 1;
+  }
+  $(".bg4").css({
+    backgroundImage: "url(" + image[curentIndex2] + ")",
+  });
+
+  console.log(image[curentIndex2]);
+});
+
+$(".next4").click(() => {
+  curentIndex2++;
+  if (curentIndex2 == image.length) {
+    curentIndex2 = 0;
+  }
+  $(".bg4").css({
+    backgroundImage: "url(" + image[curentIndex2] + ")",
+  });
+  console.log(image[curentIndex2]);
 });
